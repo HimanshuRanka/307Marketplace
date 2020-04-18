5b0f9042e71e12da0d6f83c4adde3b89b0330089
Himanshu Ranka <himanshuranka2612@gmail.com>
2020-04-18 02:35:22 -0400

    The basic login process up until the redirect has been completed

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from marketplace import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.user_login, name='user_login')
 ]
---
+++ marketplace/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from django.urls import reverse
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('templates/browse'))
+            else:
+                return HttpResponse("Your account was inactive.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username,password))
+            return HttpResponse("Invalid login details given")
+    else:
+        return render(request, 'login.html', {})
