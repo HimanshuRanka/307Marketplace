2d01b7c25f369ed4446df2aa2aec25083b2d9af9
JamieJNPC <coffeyja@tcd.ie>
2020-04-21 01:50:02 +0100

    Models 1.0 Implemented

--- Market/models.py
+++ Market/models.py
@@ -13,8 +13,24 @@ class Product(models.Model):
     # Not sure about the user attribute as
     # I havent looked into models as much so am not sure about the relationshi
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.CharField(max_length=50)
+    product = models.CharField(max_length = 50)
     product_picture = models.ImageField(upload_to=get_upload_path)
-    description = models.CharField(max_length=50)
+    description = models.CharField(max_length = 50)
     price = models.IntegerField()
     stock = models.IntegerField()
+
+
+class User(models.Model):
+	first_name = models.CharField(max_length = 20)
+	last_name = models.CharField(max_length = 20)
+	username = models.CharField(max_length = 40)
+	password = models.CharField(max_length = 40)
+	email = models.CharField(max_length = 40)
+
+class Purchase(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Address(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	address = models.CharField(max_length = 100)
