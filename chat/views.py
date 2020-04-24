93a76b899bf2cca2802be07eb8897a205a5aa114
JamieJNPC <coffeyja@tcd.ie>
2020-04-24 23:55:21 +0100

    Chat implemented but ugly

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-def index(request):
+@login_required
+def chat_index(request):
     return render(request, 'chat/index.html', {})
 
-def room(request, room_name=None):
+@login_required
+def chat_room(request, room_name=None):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
-    
